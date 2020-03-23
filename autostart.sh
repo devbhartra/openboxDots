06803c1f62e875fdd559a2e92fc4737706594862
Dev Bhartra <devbhartra183@gmail.com>
2020-03-23 21:02:26 +0530

    Added OB3 files

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+# ~/.config/openbox/autostart.sh
+#
+# Run these programs when openbox starts.
+
+# Use feh for background wallpaper.
+sh ~/.fehbg &
+
+# tint2 -- provides panel, systray, clock & taskbar
+tint2 &
+
+#wifi stuff i guess
+nm-applet &
+
+
