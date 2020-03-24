6e4f387fea48764122d1825524921436031bad04
Dev Bhartra <devbhartra183@gmail.com>
2020-03-24 17:03:59 +0530

    Added entire configuration

---
+++ openbox/autostart.sh
@@ -0,0 +1,18 @@
+# ~/.config/openbox/autostart.sh
+#
+# Run these programs when openbox starts.
+
+# Use feh for background wallpaper.
+#sh ~/.fehbg &
+
+# tint2 -- provides panel, systray, clock & taskbar
+#tint2 &
+
+#wifi stuff i guess
+nm-applet &
+
+#using nitrogen for wallpaper
+nitrogen --restore &
+
+#running polybar
+~/.config/polybar/launch.sh
